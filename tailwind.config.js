844b831ab229c3385078deac291d86a9d1ff87bc
Shun <ter0123540@gmail.com>
2022-11-24 21:04:51 +0800

    style: 設定共用顏色 set shared color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-01': '#2B5F75',
+        'main-02': '#A8D8B9',
+        'main-03': '#268785',
+        danger: '#E3265F',
+        success: '#26E39F',
+        highlight: '#F4A7B9',
+      },
+    },
   },
   plugins: [],
 };
