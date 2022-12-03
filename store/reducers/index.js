3a5da17dab855c2725b6459353840caf98382cb9
Alex Lau <alex.wanna.nirvana@gmail.com>
2022-12-03 20:15:57 +0800

    update: 新增homepage reducer; 整合reducer 加入hydrate action

--- store/reducers/index.js
+++ store/reducers/index.js
@@ -1,12 +1,28 @@
 import { combineReducers } from 'redux';
+import { HYDRATE } from 'next-redux-wrapper';
 import historyReducer from './historyReducer';
 import objectReducer from './objectReducer';
 import loginReducer from './loginReducer';
 import loaderReducer from './loaderReducer';
+import homepageReducer from './homepageReducer';
 
-export default combineReducers({
+const combinedReducer = combineReducers({
   history: historyReducer,
   canvasObject: objectReducer,
   loginStatus: loginReducer,
   loaderStatus: loaderReducer,
+  homepage: homepageReducer,
 });
+
+const reducer = (state, action) => {
+  if (action.type === HYDRATE) {
+    const nextState = {
+      ...state, // use previous state
+      ...action.payload, // apply delta from hydration
+    };
+    return nextState;
+  }
+  return combinedReducer(state, action);
+};
+
+export default reducer;
