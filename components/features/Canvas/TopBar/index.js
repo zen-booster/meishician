d5b691b987163b96719721a73d73e7cff0cdd849
Shun <ter0123540@gmail.com>
2022-12-05 15:06:49 +0800

    feat: 新增發布警告 add publish warning

--- components/features/Canvas/TopBar/index.js
+++ components/features/Canvas/TopBar/index.js
@@ -1,8 +1,10 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { FaBold, FaItalic, FaUnderline, FaTrashAlt } from 'react-icons/fa';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import Modal from '../../../common/Modal/Modal';
+import Button from '../../../common/Button/Button';
 import { fabricContext } from '../Canvas';
 import useForceUpdate from '../../../../hooks/useForceUpdate';
 import updateHistory from '../service/updateHistory';
@@ -16,6 +18,7 @@ function TopBar() {
   const canvasRef = useContext(fabricContext);
   const { activeObject } = useSelector((state) => state.canvasObject);
   const { history } = useSelector((state) => state);
+  const [showWarn, setShowWarn] = useState(false);
   const dispatch = useDispatch();
   const forceUpdate = useForceUpdate();
   const { cardId } = router.query;
@@ -97,144 +100,180 @@ function TopBar() {
 
   const publish = () => {
     dispatch(publishCanvas(cardId, canvasRef, history));
+    setShowWarn(false);
   };
 
   return (
-    <div className="z-10 flex w-full items-center justify-between gap-4 bg-gray-02 py-1.5 pl-7 pr-10 text-rwd-body text-main-01 shadow-01">
-      <div className="flex h-full gap-6">
-        <label className="relative flex h-full cursor-pointer flex-col items-center">
-          <Image
-            src="/palette.svg"
-            width={32}
-            height={32}
-            alt="palette"
-            className="my-auto"
-          />
-          <span>顏色</span>
-          <input
-            className="invisible absolute bottom-0"
-            type="color"
-            value={activeObject.fill}
-            onChange={changeColor}
-          />
-        </label>
+    <>
+      <div className="z-10 flex w-full items-center justify-between gap-4 bg-gray-02 py-1.5 pl-7 pr-10 text-rwd-body text-main-01 shadow-01">
+        <div className="flex h-full gap-6">
+          <label className="relative flex h-full cursor-pointer flex-col items-center">
+            <Image
+              src="/palette.svg"
+              width={32}
+              height={32}
+              alt="palette"
+              className="my-auto"
+            />
+            <span>顏色</span>
+            <input
+              className="invisible absolute bottom-0"
+              type="color"
+              value={activeObject.fill}
+              onChange={changeColor}
+            />
+          </label>
 
-        <button
-          className="flex h-full flex-col items-center"
-          type="button"
-          onClick={() => {
-            undo(canvasRef.current, history, dispatch);
-          }}
-        >
-          <Image
-            src="/undo.svg"
-            width={40}
-            height={18}
-            alt="undo"
-            className="my-auto"
-          />
-          <p>上一步</p>
-        </button>
-        <button
-          className="flex h-full flex-col items-center"
-          type="button"
-          onClick={() => {
-            redo(canvasRef.current, history, dispatch);
-          }}
-        >
-          <Image
-            src="/redo.svg"
-            width={40}
-            height={18}
-            alt="redo"
-            className="my-auto"
-          />
-          <p>還原</p>
-        </button>
+          <button
+            className="flex h-full flex-col items-center"
+            type="button"
+            onClick={() => {
+              undo(canvasRef.current, history, dispatch);
+            }}
+          >
+            <Image
+              src="/undo.svg"
+              width={40}
+              height={18}
+              alt="undo"
+              className="my-auto"
+            />
+            <p>上一步</p>
+          </button>
+          <button
+            className="flex h-full flex-col items-center"
+            type="button"
+            onClick={() => {
+              redo(canvasRef.current, history, dispatch);
+            }}
+          >
+            <Image
+              src="/redo.svg"
+              width={40}
+              height={18}
+              alt="redo"
+              className="my-auto"
+            />
+            <p>還原</p>
+          </button>
 
-        <button
-          type="button"
-          className="flex h-full flex-col items-center"
-          onClick={setForward}
-        >
-          <Image
-            src="/set-front.svg"
-            width={20}
-            height={31}
-            alt="set-front"
-            className="my-auto"
-          />
-          上移
-        </button>
-        <button
-          type="button"
-          className="flex h-full flex-col items-center"
-          onClick={setBackward}
-        >
-          <Image
-            src="/set-back.svg"
-            width={20}
-            height={32}
-            alt="set-back"
-            className="my-auto"
-          />
-          下移
-        </button>
-        <div className="my-auto h-12 w-0.5 bg-gray-01" />
-      </div>
+          <button
+            type="button"
+            className="flex h-full flex-col items-center"
+            onClick={setForward}
+          >
+            <Image
+              src="/set-front.svg"
+              width={20}
+              height={31}
+              alt="set-front"
+              className="my-auto"
+            />
+            上移
+          </button>
+          <button
+            type="button"
+            className="flex h-full flex-col items-center"
+            onClick={setBackward}
+          >
+            <Image
+              src="/set-back.svg"
+              width={20}
+              height={32}
+              alt="set-back"
+              className="my-auto"
+            />
+            下移
+          </button>
+          <div className="my-auto h-12 w-0.5 bg-gray-01" />
+        </div>
 
-      {activeObject.id !== 'background' && (
         <FaTrashAlt
           onClick={() => {
             removeObject(canvasRef.current, dispatch);
           }}
           className="h-6 w-6 cursor-pointer"
         />
-      )}
 
-      {activeObject.get('type') === 'textbox' && (
-        <>
-          <input
-            type="number"
-            className="w-16 text-center"
-            value={activeObject.fontSize}
-            onChange={changeSize}
-          />
-          <select onChange={textAlign} value={activeObject.textAlign}>
-            <option value="left">left</option>
-            <option value="center">center</option>
-            <option value="right">right</option>
-            <option value="justify">justify</option>
-          </select>
-          <FaBold onClick={toggleBold} className="h-6 w-6 cursor-pointer" />
-          <FaItalic onClick={toggleItalic} className="h-6 w-6 cursor-pointer" />
-          <FaUnderline
-            onClick={toggleUnderline}
-            className="h-6 w-6 cursor-pointer"
-          />
-          <select onChange={selectFont} value={activeObject.fontFamily}>
-            <option value="Times New Roman">Times New Roman</option>
-            <option value="Poppins">Poppins</option>
-            <option value="Noto Sans TC">Noto Sans TC</option>
-            <option value="Noto Serif TC">Noto Serif TC</option>
-            <option value="Xanh Mono">Xanh Mono</option>
-          </select>
-        </>
-      )}
-      <div className="flex gap-7">
-        <button type="button">預覽</button>
-        <button type="button" onClick={save}>
-          儲存
-        </button>
-        <button
-          type="button"
-          className="w-[7.5rem] rounded-xl bg-main-02 px-5 py-2 text-rwd-h5 font-bold"
-          onClick={publish}
-        >
-          發布名片
-        </button>
+        {activeObject.get('type') === 'textbox' && (
+          <>
+            <input
+              type="number"
+              className="w-16 text-center"
+              value={activeObject.fontSize}
+              onChange={changeSize}
+            />
+            <select onChange={textAlign} value={activeObject.textAlign}>
+              <option value="left">left</option>
+              <option value="center">center</option>
+              <option value="right">right</option>
+              <option value="justify">justify</option>
+            </select>
+            <FaBold onClick={toggleBold} className="h-6 w-6 cursor-pointer" />
+            <FaItalic
+              onClick={toggleItalic}
+              className="h-6 w-6 cursor-pointer"
+            />
+            <FaUnderline
+              onClick={toggleUnderline}
+              className="h-6 w-6 cursor-pointer"
+            />
+            <select onChange={selectFont} value={activeObject.fontFamily}>
+              <option value="Times New Roman">Times New Roman</option>
+              <option value="Poppins">Poppins</option>
+              <option value="Noto Sans TC">Noto Sans TC</option>
+              <option value="Noto Serif TC">Noto Serif TC</option>
+              <option value="Xanh Mono">Xanh Mono</option>
+            </select>
+          </>
+        )}
+        <div className="flex gap-7">
+          <button type="button">預覽</button>
+          <button type="button" onClick={save}>
+            儲存
+          </button>
+          <button
+            type="button"
+            className="w-[7.5rem] rounded-xl bg-main-02 px-5 py-2 text-rwd-h5 font-bold"
+            onClick={() => {
+              setShowWarn(true);
+            }}
+          >
+            發布名片
+          </button>
+        </div>
       </div>
-    </div>
+
+      {showWarn && (
+        <Modal show={setShowWarn}>
+          <div className="flex flex-col items-center font-bold text-main-01">
+            <Image
+              src="/warn.svg"
+              width={180}
+              height={221}
+              alt="warning"
+              className="mb-9"
+            />
+            <p className="mb-6 text-h4">準備發布名片了嗎？</p>
+            <p className="mb-10 text-fs-6">畫布內容將會重製</p>
+            <div className="flex gap-12 text-fs-6">
+              <Button className="w-36 bg-main-01" onClick={publish}>
+                確定發布
+              </Button>
+              <Button
+                variant="outlined"
+                className="w-36 bg-white"
+                onClick={() => {
+                  setShowWarn(false);
+                }}
+              >
+                取消
+              </Button>
+            </div>
+          </div>
+        </Modal>
+      )}
+    </>
   );
 }
 
