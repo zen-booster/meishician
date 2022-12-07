8b496bed3549511492cc4b1d655886eb8313956c
Shun <ter0123540@gmail.com>
2022-12-07 13:28:57 +0800

    refactor: 完成 flip 重構

--- components/features/Canvas/service/flip.js
+++ components/features/Canvas/service/flip.js
@@ -1,27 +1,45 @@
-import { NO_UPDATE, UPDATE } from '../../../../constants/constants';
+import {
+  FLIP,
+  NO_UPDATE,
+  NEED_UPDATE,
+  UPDATE,
+  SET_ACTIVE,
+} from '../../../../constants/constants';
+import getBackground from './getBackground';
 import serialize from './serialize';
-import loadCanvas from './loadCanvas';
+import setLoadData from './setLoadData';
 
 function flip(canvas, history, dispatch) {
   const { front, back, position } = history.state;
   const serializedData = serialize(canvas);
-  const order = { orderName: 'flip', dispatch };
   dispatch({
     type: UPDATE,
-    payload: {
-      newState: serializedData,
-    },
+    payload: { newState: serializedData },
   });
 
   dispatch({ type: NO_UPDATE });
-  switch (position) {
-    case 'front':
-      loadCanvas(canvas, back, order);
-      break;
-    case 'back':
-      loadCanvas(canvas, front, order);
-      break;
-    default:
+
+  if (position === 'front') {
+    const loadData = setLoadData(canvas, back);
+    canvas.loadFromJSON(loadData, () => {
+      dispatch({ type: NEED_UPDATE });
+      dispatch({ type: FLIP });
+      const background = getBackground(canvas);
+      canvas.clipPath = background;
+      canvas.renderAll();
+      dispatch({ type: SET_ACTIVE, payload: background });
+    });
+  }
+  if (position === 'back') {
+    const loadData = setLoadData(canvas, front);
+    canvas.loadFromJSON(loadData, () => {
+      dispatch({ type: NEED_UPDATE });
+      dispatch({ type: FLIP });
+      const background = getBackground(canvas);
+      canvas.clipPath = background;
+      canvas.renderAll();
+      dispatch({ type: SET_ACTIVE, payload: background });
+    });
   }
 }
 
