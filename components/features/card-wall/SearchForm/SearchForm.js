943fbb9a609210911ae91f6f08d775801aa413bc
Shun <ter0123540@gmail.com>
2022-12-08 15:50:56 +0800

    feat: 完成 card-wall 頁 api style: 調整 card-wall 頁樣式

--- components/features/card-wall/SearchForm/SearchForm.js
+++ components/features/card-wall/SearchForm/SearchForm.js
@@ -1,8 +1,6 @@
 import { useForm, Controller } from 'react-hook-form';
 import Image from 'next/image';
 import SearchInput from '../../../common/Input/SearchInput';
-import Select from '../../../common/Select/Select';
-// import Button from '../../../common/Button/Button';
 import domainData from '../../../../data/domainData';
 import areaData from '../../../../data/areaData';
 
@@ -16,37 +14,46 @@ function SearchForm() {
   const allDomain = [all, ...domainData];
   const allArea = [all, ...areaData];
 
-  const { control, handleSubmit } = useForm();
+  const { register, control, handleSubmit } = useForm();
   const onSubmit = (data) => console.log(data);
+
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="mx-auto w-204 rounded-xl bg-white px-16 pb-16 pt-6 shadow-frame"
+      className="mx-auto max-w-container rounded-xl bg-white pb-8 pt-4 shadow-01"
     >
-      <h3 className="mb-8 text-h5 text-main-01">搜尋名片</h3>
+      <h3 className="mb-8 text-center text-h5 font-bold text-main-01">
+        搜尋名片
+      </h3>
 
-      <div className="flex gap-6">
-        <Controller
-          control={control}
-          name="domain"
-          defaultValue="all"
-          render={({ field: { onChange, value, name } }) => (
-            <Select onChange={onChange} value={value} name={name}>
-              {allDomain}
-            </Select>
-          )}
-        />
-        <Controller
-          control={control}
-          name="city"
-          defaultValue="all"
-          render={({ field: { onChange, value, name } }) => (
-            <Select onChange={onChange} value={value} name={name}>
-              {allArea}
-            </Select>
-          )}
-        />
-        <div className="flex flex-1">
+      <div className="flex h-12 w-full justify-center gap-6">
+        <select
+          {...register('domain')}
+          className="w-44 rounded-lg border border-dark-light bg-white px-2 text-dark-light"
+        >
+          {allDomain.map((domain) => (
+            <option
+              key={domain.value}
+              value={domain.value}
+              className="text-black"
+            >
+              {domain.content}
+            </option>
+          ))}
+        </select>
+
+        <select
+          {...register('area')}
+          className="w-44 rounded-lg border border-dark-light bg-white px-2 text-dark-light"
+        >
+          {allArea.map((area) => (
+            <option key={area.value} value={area.value}>
+              {area.content}
+            </option>
+          ))}
+        </select>
+
+        <div className="flex w-96">
           <Controller
             control={control}
             name="search"
@@ -56,12 +63,12 @@ function SearchForm() {
                 onChange={onChange}
                 value={value}
                 name={name}
-                placeholder="請輸入名片ID"
+                placeholder="名字（選填）"
               />
             )}
           />
           <button
-            className="flex h-12 basis-36 items-center justify-center rounded-r-xl bg-main-01"
+            className="flex basis-36 items-center justify-center rounded-r-xl bg-main-01"
             type="submit"
           >
             <Image src="/search.svg" alt="search-icon" width={18} height={18} />
