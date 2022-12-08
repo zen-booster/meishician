93922400cd3f289500c073eae4485bdceed35f95
Shun <ter0123540@gmail.com>
2022-12-08 21:00:16 +0800

    style: 更改 Navbar 樣式

--- components/common/Navbar/Navbar.jsx
+++ components/common/Navbar/Navbar.jsx
@@ -42,27 +42,27 @@ function Navbar({ children }) {
 
   return (
     <>
-      <div className="fixed z-30 flex h-16 w-full items-center justify-between bg-main-01 px-5 py-2 text-white shadow-01">
+      <div className="fixed z-30 flex h-16 w-full items-center justify-between bg-main-01 py-2 pl-8 pr-11 font-bold text-white shadow-01">
         <div className="flex items-center gap-14 ">
           <Link href="/">
             <Image src="/logo-dark.png" alt="logo" width={161} height={65} />
           </Link>
           <span className="hidden laptop:block">電子名片商務方案</span>
         </div>
-        <ul className="flex items-center">
-          <li className="hidden laptop:block laptop:h-full laptop:px-6">
+        <ul className="flex items-center gap-12">
+          <li className="hidden laptop:block laptop:h-full">
             <Link href="/card-wall">名片牆</Link>
           </li>
 
           {isLogin && (
             <>
-              <li className="hidden laptop:block laptop:cursor-pointer laptop:px-6">
+              <li className="hidden laptop:block laptop:cursor-pointer">
                 <Link href="/add-card">打造名片</Link>
               </li>
-              <li className="hidden laptop:block laptop:cursor-pointer laptop:px-6">
+              <li className="hidden laptop:block laptop:cursor-pointer">
                 <Link href="management">管理名片</Link>
               </li>
-              <li className="cursor-pointer px-4 py-1 laptop:cursor-pointer laptop:px-6">
+              <li className="cursor-pointer py-1 laptop:cursor-pointer">
                 <Link href="/notification">
                   <Image
                     className="cursor-pointer"
@@ -74,14 +74,19 @@ function Navbar({ children }) {
                 </Link>
               </li>
               <li
-                className="cursor-pointer px-4 py-1 laptop:hidden laptop:cursor-pointer laptop:px-6"
+                className="cursor-pointer py-1 laptop:hidden laptop:cursor-pointer"
                 onClick={toggleDrawer}
                 ref={drawerRef}
               >
-                <MdMenu className="text-sm" />
+                <Image
+                  src="/hamburger-bar.svg"
+                  width={18}
+                  height={12}
+                  alt="hamburger-bar"
+                />
               </li>
               <li
-                className="hidden laptop:relative laptop:block laptop:cursor-pointer laptop:px-6"
+                className="hidden laptop:relative laptop:block laptop:cursor-pointer"
                 onClick={toggleExtra}
                 ref={avatarRef}
               >
@@ -117,20 +122,35 @@ function Navbar({ children }) {
             <>
               <li className="hidden laptop:block">
                 <Link href="/login">
-                  <Button>Login</Button>
+                  <button
+                    type="button"
+                    className="w-20 rounded-md bg-main-02 py-1 text-main-01"
+                  >
+                    註冊
+                  </button>
                 </Link>
               </li>
               <li className="hidden laptop:block">
                 <Link href="sign-up">
-                  <Button>Sign Up</Button>
+                  <button
+                    type="button"
+                    className="w-20 rounded-md border border-white bg-main-01 py-1 text-white "
+                  >
+                    登入
+                  </button>
                 </Link>
               </li>
               <li
-                className="cursor-pointer px-8 py-1 laptop:hidden"
+                className="cursor-pointer py-1 laptop:hidden"
                 onClick={toggleDrawer}
                 ref={drawerRef}
               >
-                <MdMenu className="text-sm" />
+                <Image
+                  src="/hamburger-bar.svg"
+                  width={18}
+                  height={12}
+                  alt="hamburger-bar"
+                />
               </li>
             </>
           )}
