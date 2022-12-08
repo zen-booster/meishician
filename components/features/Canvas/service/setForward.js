f345fa215d2cb0bf368e469461d90c95413a17fd
Shun <ter0123540@gmail.com>
2022-12-08 10:24:29 +0800

    fix: 修正 setForward 影響 history 的 bug

--- components/features/Canvas/service/setForward.js
+++ components/features/Canvas/service/setForward.js
@@ -2,6 +2,11 @@ import updateHistory from './updateHistory';
 
 function setForward(canvas, activeObject, dispatch) {
   if (activeObject.id === 'background') return;
+  if (
+    canvas.getObjects().indexOf(activeObject) ===
+    canvas.getObjects().length - 1
+  )
+    return;
   canvas.bringForward(activeObject);
   canvas.renderAll();
   updateHistory(canvas, dispatch);
