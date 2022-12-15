cc22e657a014d033bd6640e83163dc354c1d7c56
Shun <ter0123540@gmail.com>
2022-12-15 13:16:53 +0800

    style: 增加 cavnas-editor 的 SideBar 動畫

--- components/features/Canvas/SideBar/Drawer/Drawer.js
+++ components/features/Canvas/SideBar/Drawer/Drawer.js
@@ -1,16 +1,90 @@
+import { MdKeyboardArrowLeft } from 'react-icons/md';
+import { motion } from 'framer-motion';
 import TextTool from './Tool/TextTool';
 import ImageTool from './Tool/ImageTool';
 import ShapeTool from './Tool/ShapeTool';
 import TemplateTool from './Tool/TemplateTool';
 
-function Drawer({ buttonName }) {
+function Drawer({ buttonName, toggleDrawer }) {
+  const containerVariants = {
+    init: {
+      width: '0px',
+    },
+    show: {
+      width: '208px',
+      transition: {
+        when: 'beforeChildren',
+        mass: 0.1,
+      },
+    },
+  };
+
+  const contentVariants = {
+    init: {
+      opacity: 0,
+    },
+    show: {
+      opacity: 1,
+    },
+    exit: {
+      opacity: 0,
+    },
+  };
+
   return (
-    <div className="z-20 flex h-full w-52 flex-col items-center bg-gray-03">
-      {buttonName === 'Text' && <TextTool />}
-      {buttonName === 'Material' && <ShapeTool />}
-      {buttonName === 'Image' && <ImageTool />}
-      {buttonName === 'Template' && <TemplateTool />}
-    </div>
+    <motion.div
+      className="relative z-20 flex h-full w-52 flex-col items-center bg-gray-03 px-3 py-4"
+      variants={containerVariants}
+      initial="init"
+      animate="show"
+    >
+      <button
+        onClick={toggleDrawer}
+        name="close"
+        type="button"
+        className="absolute top-1/2 left-full -translate-y-1/2 rounded-r-xl bg-gray-03 py-5"
+      >
+        <MdKeyboardArrowLeft className="pointer-events-none text-h5 text-white" />
+      </button>
+
+      {buttonName === 'Template' && (
+        <motion.div
+          className="w-full"
+          variants={contentVariants}
+          exit={{ opacity: 0 }}
+        >
+          <TemplateTool />
+        </motion.div>
+      )}
+
+      {buttonName === 'Text' && (
+        <motion.div
+          className="w-full"
+          variants={contentVariants}
+          exit={{ opacity: 0 }}
+        >
+          <TextTool />
+        </motion.div>
+      )}
+      {buttonName === 'Material' && (
+        <motion.div
+          className="w-full"
+          variants={contentVariants}
+          exit={{ opacity: 0 }}
+        >
+          <ShapeTool />
+        </motion.div>
+      )}
+      {buttonName === 'Image' && (
+        <motion.div
+          className="w-full"
+          variants={contentVariants}
+          exit={{ opacity: 0 }}
+        >
+          <ImageTool />
+        </motion.div>
+      )}
+    </motion.div>
   );
 }
 
--- components/features/Canvas/SideBar/Drawer/Tool/ShapeTool.js
+++ components/features/Canvas/SideBar/Drawer/Tool/ShapeTool.js
@@ -5,17 +5,17 @@ import Line from '../../../module/Line';
 
 function ShapeTool() {
   return (
-    <ul className="flex w-full flex-wrap items-center justify-center gap-4 px-4 py-4 text-3xl">
-      <li className="flex h-28 w-28 cursor-pointer items-center justify-center">
+    <ul className="flex w-full flex-wrap items-center justify-center gap-4 text-3xl">
+      <li className="flex h-28 w-28 cursor-pointer items-center justify-center duration-200 ease-in hover:scale-110">
         <Rectangle />
       </li>
-      <li className="flex h-28 w-28 cursor-pointer items-center justify-center">
+      <li className="flex h-28 w-28 cursor-pointer items-center justify-center duration-200 ease-in hover:scale-110">
         <Circle />
       </li>
-      <li className="flex h-28 w-28 cursor-pointer items-center justify-center">
+      <li className="flex h-28 w-28 cursor-pointer items-center justify-center duration-200 ease-in hover:scale-110">
         <Triangle />
       </li>
-      <li className="flex h-28 w-28 cursor-pointer items-center justify-center">
+      <li className="flex h-28 w-28 cursor-pointer items-center justify-center duration-200 ease-in hover:scale-110">
         <Line />
       </li>
     </ul>
