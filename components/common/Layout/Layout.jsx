b16b6b6d6864a35ca9e56484f543a21d7ee51e4b
Alex Lau <alex.wanna.nirvana@gmail.com>
2022-12-20 00:09:11 +0800

    update: Layout Footer 選配設定

--- components/common/Layout/Layout.jsx
+++ components/common/Layout/Layout.jsx
@@ -1,13 +1,23 @@
 import { useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
 import Navbar from '../Navbar/Navbar';
 import Loader from '../Loader/Loader';
 
 function Layout({ children }) {
+  const router = useRouter();
+
+  function startsWithAny(currentPath, pathStringArr) {
+    return pathStringArr.map((s) => currentPath.startsWith(s)).includes(true);
+  }
+  const pageWithFooter = ['/card-wall', '/homepage', '/notification'];
   const { isLoading } = useSelector((state) => state.loaderStatus);
   return (
     <>
       <Navbar />
       {children}
+
+      {startsWithAny(router.pathname, pageWithFooter) && <Footer />}
+
       {isLoading && <Loader />}
     </>
   );
